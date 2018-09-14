d9520da200dc944a26abeaeaab89298af7349543
goldminer1030 <goldminer1030@gmail.com>
2018-09-14 17:47:01 +0200

    Removed the fontawesome links

--- index.php
+++ index.php
@@ -409,7 +409,6 @@
     * */
    (function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);</script>
     <script type="text/javascript" async src="vendor/jQuery.YoutubeBackground/jquery.youtubebackground.min.js"></script>
-    <script type="text/javascript" async src="https://use.fontawesome.com/bc8520503f.js"></script>
     <script type="text/javascript" async src="https://www.youtube.com/iframe_api"></script>
     <script type="text/javascript" async src="vendor/fullpage/fullpage.min.js"></script>
     <script type="text/javascript" async src="vendor/isotope/isotope.pkgd.min.js"></script>
